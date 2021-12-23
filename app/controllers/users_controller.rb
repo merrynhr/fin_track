d6d3b0b85ed2f1908d707451d32dee037c16e761
Merryn <merrynhr@gmail.com>
2021-12-23 12:49:11 +0000

    search friends with ajax

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,25 @@ class UsersController < ApplicationController
     @friends_list = current_user.friends
   end
 
+  def search 
+    if params[:friend].present?
+      @friend = params[:friend]
+      if @friend
+        respond_to do |format|
+          format.js { render partial: 'users/friend_result' }
+        end
+      else     
+        respond_to do |format|
+          flash.now[:alert] = "User not found"
+          format.js { render partial: 'users/friend_result' }
+        end
+      end
+    else   
+      respond_to do |format|
+        flash.now[:alert] = "Please enter a name or email"
+        format.js { render partial: 'users/friend_result' }
+      end
+    end
+  end
+
 end

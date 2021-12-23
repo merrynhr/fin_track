fedabddeea51b422e683a15b963b2134d8d09d7e
Merryn <merrynhr@gmail.com>
2021-12-23 17:52:31 +0000

    generated friendships controller and implemented create and destroyactions for friends

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   get 'search_stock', to: 'stocks#search'
   get 'friends_list', to: 'users#friends_list'
   get 'search_friend', to: 'users#search'
+  resources :friendships, only: [:create, :destroy]
+
 end
